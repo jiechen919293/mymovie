dc23f737a848a63533277803faeba4c0f9910455
Jie Chen <chenjie919293@gmail.com>
2021-06-20 12:24:22 -0500

    detailsonClick

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,8 +1,27 @@
-// import { useLocation } from "react-router-dom"
+import React, { useState } from 'react';
 const Details = (props) => {
-    let movie=props.location.query
+    const [watchList, setWatchList] = useState(JSON.parse(localStorage.getItem("watchList")) || []);
+
+    //set initial value
+    let movie = props.location.query || {}
     console.log(movie);
 
+    //function for handle watchlist
+    const handleWatch = (movie) => {
+        let tempList = [...watchList];
+        let index = watchList.findIndex((item) => {
+            return item.id === movie.id;
+        })
+        if (index === -1) {
+            tempList = [...watchList, movie]
+        }
+        else {
+            tempList.splice(index, 1)
+        }
+        setWatchList(tempList)
+        localStorage.setItem('watchList', JSON.stringify(tempList))
+        // console.log(watchList);
+    }
     return (
         <div className="show-details">
             <img src={movie.poster_path ? `https://image.tmdb.org/t/p/original/${movie.backdrop_path}` : `./image-not-available.jpg`} alt="post movie" />
@@ -11,14 +30,12 @@ const Details = (props) => {
                 <div className="description">
                     {movie.overview}
                 </div>
-                <button className="add-to-watchlist">+ Add to watch list</button>
-                {/* <button
-                 className={testWatch.find((x)=>{return x.id===movie.id})? "remove-to-watchlist" : "add-to-watchlist"}
-                 onClick={handleWatch}
+                <button
+                    className={watchList.find((x) => { return x.id === movie.id }) ? "remove-to-watchlist" : "add-to-watchlist"}
+                    onClick={() => handleWatch(movie)}
                 >
-                {testWatch.find((x)=>{return x.id===movie.id})? `- Remove from watch list`:`+ Add to watch list`}
-                </button> */}
-
+                    {watchList.find((x) => { return x.id === movie.id }) ? `- Remove from watch list` : `+ Add to watch list`}
+                </button>
             </div>
         </div >
     );
